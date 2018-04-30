e4eb5ffcc33bbe2cc923f34254f85a6c7f7327bd
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-04-30 12:29:54 -0700

    Finished beesbeesbees sprint and passed all tests.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
 var Bee = function() {
+  this.age = 5;
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+Bee.prototype.job = 'keep on growing';
+Bee.prototype.color = 'yellow';
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,11 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.honeyPot = 0;
+HoneyMakerBee.prototype.makeHoney = function () { this.honeyPot++;};
+HoneyMakerBee.prototype.giveHoney = function () { this.honeyPot--;};
